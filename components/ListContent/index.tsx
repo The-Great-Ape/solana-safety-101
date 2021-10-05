75886296c8c5ee6b3eaf87a54149bd9f7cf2b77d
SULTAN PEYEK <sultanpeyek999@gmail.com>
2021-10-05 17:53:11 +0700

    Fix layout using CSS column

--- components/ListContent/index.tsx
+++ components/ListContent/index.tsx
@@ -6,22 +6,24 @@ import ListHeader from "../ListHeader";
 const ListContent = (props: any) => {
   const { data } = props;
   return (
-    <div className={styles.container}>
+    <>
       <div className={styles.headerContainer}>
         <ListHeader />
         <ListHeader />
         <ListHeader />
       </div>
-      <div className={styles.innerContainer}>
-        {data.map((items: any, key: any) => (
-          <div key={key} className={styles.groupContainer}>
-            {items.map((item: any, key: any) => (
-              <ListItem key={key} {...item} />
-            ))}
-          </div>
-        ))}
+      <div className={styles.container}>
+        <div className={styles.innerContainer}>
+          {data.map((items: any, key: any) => (
+            <div key={key} className={styles.groupContainer}>
+              {items.map((item: any, key: any) => (
+                <ListItem key={key} {...item} />
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default ListContent;
